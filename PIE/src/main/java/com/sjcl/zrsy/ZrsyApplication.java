af5485be5be716c4a0f430bdf2e826285c66afec
Jerry <407847057@qq.com>
2018-08-10 15:37:22 +0800

    转移到idea

---
+++ PIE/src/main/java/com/sjcl/zrsy/ZrsyApplication.java
@@ -0,0 +1,13 @@
+package com.sjcl.zrsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZrsyApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZrsyApplication.class, args);
+    }
+}
